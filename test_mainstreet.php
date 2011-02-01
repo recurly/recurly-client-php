ba23bc54612ab7bcf5b991490259463760df6f60
Isaac Hall <isaac@isaachall.com>
2011-02-01 01:15:46 -0800

    Removed test file

--- test_mainstreet.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once 'library/recurly.php';
-RecurlyClient::SetAuth('support@mainstreethub.com', '85a24ac1a85b49b394cf3fb6f655cbb1', 'mainstreethub');
-
-
-$bi = RecurlyBillingInfo::getBillingInfo('56805');
-
-print_r($bi);
