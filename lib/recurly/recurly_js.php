b987d67541055f94e4a00678202ff5541f2c9810
emery <emery.denuccio@gmail.com>
2012-01-17 16:00:34 -0800

    recurly.js v2 support

--- lib/recurly/recurly_js.php
+++ lib/recurly/recurly_js.php
@@ -27,24 +27,7 @@ class Recurly_js
     if (!isset($this->data['signature'], $this->data['account_code'], $this->data['plan_code'], $this->data['quantity']))
       throw new InvalidArgumentException("Signature, account_code, plan_code, and/or quantity not present.");
 
-    $verify_data = array(
-      'account_code' => $this->data['account_code'],
-      'plan_code' => $this->data['plan_code'],
-      'quantity' => $this->data['quantity']
-    );
-
-    if (isset($this->data['coupon_code']))
-      $verify_data['coupon_code'] = $this->data['coupon_code'];
-
-    if (isset($this->data['add_ons']) && is_array($this->data['add_ons'])) {
-      $add_ons = array();
-      foreach ($this->data['add_ons'] as $add_on) {
-        $add_ons[] = array('add_on_code' => $add_on['add_on_code'], 'quantity' => $add_on['quantity']);
-      }
-      $verify_data['add_ons'] = $add_ons;
-    }
-
-    return $this->_verifyResults(self::SUBSCRIPTION_CREATED, $this->data['signature'], $verify_data);
+    return $this->_verifyResults(self::SUBSCRIPTION_CREATED, $this->data);
   }
 
   public function verifyTransaction()
@@ -52,13 +35,7 @@ class Recurly_js
     if (!isset($this->data['signature'], $this->data['account_code'], $this->data['amount_in_cents'], $this->data['currency'], $this->data['uuid']))
       throw new InvalidArgumentException("Signature, account_code, amount_in_cents, currency, and/or uuid not present.");
 
-    return $this->_verifyResults(self::TRANSACTION_CREATED, $this->data['signature'],
-      array(
-        'account_code' => $this->data['account_code'],
-        'amount_in_cents' => $this->data['amount_in_cents'],
-        'currency' => $this->data['currency'],
-        'uuid' => $this->data['uuid']
-      ));
+    return $this->_verifyResults(self::TRANSACTION_CREATED, $this->data);
   }
 
   public function verifyBillingInfoUpdated()
@@ -66,7 +43,7 @@ class Recurly_js
     if (!isset($this->data['signature'], $this->data['account_code']))
       throw new InvalidArgumentException("Signature and account_code not present.");
 
-    return $this->_verifyResults(self::BILLING_INFO_UPDATED, $this->data['signature'], array('account_code' => $this->data['account_code']));
+    return $this->_verifyResults(self::BILLING_INFO_UPDATED, $this->data);
   }
 
   // Create a signature for a one-time transaction for the given $accountCode
@@ -81,6 +58,15 @@ class Recurly_js
       'account_code' => $accountCode,
       'amount_in_cents' => $amountInCents,
       'currency' => $currency
+    ));
+  }
+
+  // Create a signature for a one-time transaction for the given $accountCode
+  public static function signSubscription($planCode, $accountCode)
+  {
+    return self::_generateSignature(self::SUBSCRIPTION_CREATE, array(
+      'plan_code' => $planCode,
+      'account_code' => $accountCode
     ));
   }
 
@@ -100,8 +86,15 @@ class Recurly_js
   }
 
   # Validate the parameters are authentic
-  private function _verifyResults($claim, $signature, $values)
+  private function _verifyResults($claim, $values)
   {
+    if(!isset($values['signature'])) {
+      throw new Recurly_ForgedQueryStringError("Mising signature");
+    }
+
+    $signature = $values['signature'];
+    unset($values['signature']);
+
     $pos = strpos($signature, '-');
     if (!$pos)
       throw new Recurly_ForgedQueryStringError("Invalid signature");
