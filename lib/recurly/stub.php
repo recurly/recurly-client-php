851a5f6b9b6d5033c264055c1caab40793abccea
Michael Beale <michael.beale@gmail.com>
2012-12-03 16:39:30 -0700

    added a more intuitive way to get stubs

--- lib/recurly/stub.php
+++ lib/recurly/stub.php
@@ -9,6 +9,7 @@ class Recurly_Stub extends Recurly_Base
    * Stubbed object type. Useful for printing the current object as a string.
    */
   var $objectType;
+  private $_data;
 
   function __construct($objectType, $href, $client = null)
   {
@@ -22,7 +23,17 @@ class Recurly_Stub extends Recurly_Base
   function get() {
     return self::_get($this->_href, $this->_client);
   }
-  
+
+  /**
+   * More intuitive way to lazily fetch stubs
+   */
+  function __get($var) {
+    if ($this->_data == null){
+      $this->_data = self::_get($this->_href, $this->_client);
+    }
+    return $this->_data->$var;
+  }
+
   public function __toString()
   {
     return "<Recurly_Stub[{$this->objectType}] href={$this->_href}>";
