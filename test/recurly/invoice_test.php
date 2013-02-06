f31c8815cb13475cc7076d6f7c5c81f424d2f5dc
andrew morton <drewish@katherinehouse.com>
2013-02-06 15:31:53 -0800

    Tests for marking success and failue on invoices.

--- test/recurly/invoice_test.php
+++ test/recurly/invoice_test.php
@@ -13,7 +13,7 @@ class Recurly_InvoiceTest extends UnitTestCase
     $invoice = Recurly_Invoice::get('abcdef1234567890', $client);
 
     $this->assertIsA($invoice, 'Recurly_Invoice');
-    $this->assertEqual($invoice->state, 'collected');
+    $this->assertEqual($invoice->state, 'open');
     $this->assertEqual($invoice->total_in_cents, 2995);
     $this->assertEqual($invoice->getHref(),'https://api.recurly.com/v2/invoices/abcdef1234567890');
     $this->assertIsA($invoice->transactions, 'Recurly_TransactionList');
@@ -37,9 +37,9 @@ class Recurly_InvoiceTest extends UnitTestCase
     $this->assertEqual($invoice->total_in_cents, 300);
     $this->assertEqual($invoice->getHref(),'https://api.recurly.com/v2/invoices/012345678901234567890123456789ab');
   }
-  
+
   public function testFailedInvoicePendingCharges()
-  {  
+  {
     $responseFixture = loadFixture('./fixtures/invoices/create-422.xml');
 
     $client = new MockRecurly_Client();
@@ -53,8 +53,42 @@ class Recurly_InvoiceTest extends UnitTestCase
     catch (Recurly_ValidationError $e)
     {
       $this->pass("Received Recurly_ValidationError");
-      
+
       $this->assertEqual($e->errors[0]->symbol,'will_not_invoice');
     }
+  }
+
+  public function testMarkSuccessful()
+  {
+    $loadFixture = loadFixture('./fixtures/invoices/show-200.xml');
+    $responseFixture = loadFixture('./fixtures/invoices/mark_successful-200.xml');
+
+    $client = new MockRecurly_Client();
+    $client->returns('request', $loadFixture, array('GET', '/invoices/abcdef1234567890'));
+    // Two things to note:
+    // - the invoice will use the full URL from the XML response
+    // - Recurly_Resource::_save() passes the current XML into the PUT which
+    //   doesn't seem quite right but I don't want to change it without
+    //   understanding what side effects it would have.
+    $client->returns('request', $responseFixture, array('PUT', 'https://api.recurly.com/v2/invoices/abcdef1234567890/mark_successful', '*'));
+
+    $invoice = Recurly_Invoice::get('abcdef1234567890', $client);
+    $invoice->markSuccessful();
+    $this->assertEqual($invoice->state, 'collected');
+  }
+
+  public function testMarkFailed()
+  {
+    $loadFixture = loadFixture('./fixtures/invoices/show-200.xml');
+    $responseFixture = loadFixture('./fixtures/invoices/mark_failed-200.xml');
+
+    $client = new MockRecurly_Client();
+    $client->returns('request', $loadFixture, array('GET', '/invoices/abcdef1234567890'));
+    // See the notes in testMarkSuccessful().
+    $client->returns('request', $responseFixture, array('PUT', 'https://api.recurly.com/v2/invoices/abcdef1234567890/mark_failed', '*'));
+
+    $invoice = Recurly_Invoice::get('abcdef1234567890', $client);
+    $invoice->markFailed();
+    $this->assertEqual($invoice->state, 'failed');
   }
 }
