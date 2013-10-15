4d7329ed80700d40d0199282a322046551d72a21
andrew morton <drewish@katherinehouse.com>
2013-10-15 10:44:21 -0700

    Remove old taxable parameter.

--- test/recurly/adjustment_test.php
+++ test/recurly/adjustment_test.php
@@ -11,4 +11,21 @@ class Recurly_AdjustmentTest extends UnitTestCase
     $this->assertIsA($adjustment, 'Recurly_Adjustment');
     $adjustment->delete();
   }
+
+  public function testXml() {
+    $charge = new Recurly_Adjustment();
+    $charge->account_code = '1';
+    $charge->description = 'Charge for extra bandwidth';
+    $charge->unit_amount_in_cents = 5000; // $50.00
+    $charge->currency = 'USD';
+    $charge->quantity = 1;
+    $charge->accounting_code = 'bandwidth';
+
+    // This deprecated parameter should be ignored:
+    $charge->taxable = 0;
+
+    $expected = '<?xml version="1.0"?>' + "\n";
+    $expected += '<adjustment><currency>USD</currency><unit_amount_in_cents>5000</unit_amount_in_cents><quantity>1</quantity><description>Charge for extra bandwidth</description><accounting_code>bandwidth</accounting_code></adjustment>';
+    $this->assertEqual($charge->xml(), $expected);
+  }
 }
