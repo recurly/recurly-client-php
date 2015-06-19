6fcf331f659e16536ac1394341e3a7c839f2ffdc
Chris Barton <chris.barton@recurly.com>
2015-06-19 16:36:48 -0700

    Add option to specify the refund_apply_order for invoice refunds

--- lib/recurly/adjustment.php
+++ lib/recurly/adjustment.php
@@ -34,15 +34,16 @@ class Recurly_Adjustment extends Recurly_Resource
    *
    * @param Integer the quantity you wish to refund, defaults to refunding the entire quantity
    * @param Boolean indicates whether you want this adjustment refund prorated
+   * @param String indicates the refund order to apply, valid options: {'credit','transaction'}, defaults to 'credit'
    * @return Recurly_Invoice the new refund invoice
    * @throws Recurly_Error if the adjustment cannot be refunded.
    */
-  public function refund($quantity = null, $prorate = false) {
+  public function refund($quantity = null, $prorate = false, $refund_apply_order = 'credit') {
     if ($this->state == 'pending') {
       throw new Recurly_Error("Only invoiced adjustments can be refunded");
     }
     $invoice = $this->invoice->get();
-    return $invoice->refund($this->toRefundAttributes($quantity, $prorate));
+    return $invoice->refund($this->toRefundAttributes($quantity, $prorate), $refund_apply_order);
   }
 
   /**
