e5b9533acce284daea5ace5630478cfad7a2192b
Laurent Sarrazin <Laurent-Sarrazin@users.noreply.github.com>
2015-10-16 10:08:25 +0200

    Update subscription states

--- lib/recurly/subscription_list.php
+++ lib/recurly/subscription_list.php
@@ -8,8 +8,24 @@ class Recurly_SubscriptionList extends Recurly_Pager
   public static function getTrials($params = null, $client = null) {
     return Recurly_SubscriptionList::get(Recurly_Pager::_setState($params, 'in_trial'), $client);
   }
-  public static function getNonRenewing($params = null, $client = null) {
-    return Recurly_SubscriptionList::get(Recurly_Pager::_setState($params, 'non_renewing'), $client);
+  public static function getCanceled($params = null, $client = null) {
+    return Recurly_SubscriptionList::get(Recurly_Pager::_setState($params, 'canceled'), $client);
+  }
+
+  public static function getExpired($params = null, $client = null) {
+    return Recurly_SubscriptionList::get(Recurly_Pager::_setState($params, 'expired'), $client);
+  }
+  
+  public static function getFuture($params = null, $client = null) {
+    return Recurly_SubscriptionList::get(Recurly_Pager::_setState($params, 'future'), $client);
+  }
+  
+  public static function getLive($params = null, $client = null) {
+    return Recurly_SubscriptionList::get(Recurly_Pager::_setState($params, 'live'), $client);
+  }
+  
+  public static function getPastDue($params = null, $client = null) {
+    return Recurly_SubscriptionList::get(Recurly_Pager::_setState($params, 'past_due'), $client);
   }
 
   public static function get($params = null, $client = null)
