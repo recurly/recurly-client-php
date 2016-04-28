d4de92e3f36065f8a55bf194c7fb75a55641e5b9
phpdave <davelhs03@yahoo.com>
2016-04-28 12:20:39 -0400

    Allow user to add or override curl options
    Added public static $curloptionsArray which should be set to an associative array of key values of curl options
    
    ```php
    //Example: the user of the client library can't upgrade their PHP version, libcurl, but need to address a problem where the OS is not negotiating to higher tls levels.
    Recurly_Client::$curloptionsArray = array(CURLOPT_SSLVERSION => 6);
    ```

--- lib/recurly/client.php
+++ lib/recurly/client.php
@@ -35,6 +35,12 @@ class Recurly_Client
   public static $CACertPath = false;
 
   /**
+   * Associtative array of curl options to add or override default curl options. Use only if needed (if you can't fix libcurl/php).
+   * Recurly_Client::$curloptionsArray = array(CURLOPT_SSLVERSION => 6);  //6 - CURL_SSLVERSION_TLSv1_2
+   */
+  public static $curloptionsArray = array();
+
+  /**
    * API Key instance, may differ from the static key
    */
   private $_apiKey;
@@ -124,7 +130,6 @@ class Recurly_Client
     curl_setopt($ch, CURLOPT_URL, $uri);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, TRUE);
     curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-    curl_setopt($ch, CURLOPT_SSLVERSION, 6);
     if (self::$CACertPath) {
       curl_setopt($ch, CURLOPT_CAINFO, self::$CACertPath);
     }
@@ -157,7 +162,12 @@ class Recurly_Client
     {
       curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
     }
-
+    
+    if(!empty($curloptionsArray))
+    {
+      curl_setopt_array($ch, $curloptionsArray);
+    }
+    
     $response = curl_exec($ch);
 
     if ($response === false)
