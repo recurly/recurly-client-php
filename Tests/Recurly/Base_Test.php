4281d70775ff27d5ce4b47f67dfc0db374a1cb32
andrew morton <drewish@katherinehouse.com>
2016-08-10 09:23:15 -0600

    Pass the client from pager to its items
    Fixes #254

--- Tests/Recurly/Base_Test.php
+++ Tests/Recurly/Base_Test.php
@@ -1,8 +1,15 @@
 <?php
 
 
-class Recurly_BaseTest extends Recurly_TestCase {
+// Do a song and dance to gain access to private/protected properties.
+function getProtectedProperty($obj, $property) {
+  $reflection = new ReflectionClass($obj);
+  $property = $reflection->getProperty($property);
+  $property->setAccessible(true);
+  return $property->getValue($obj);
+}
 
+class Recurly_BaseTest extends Recurly_TestCase {
   public function testParsingEmptyXML() {
     $this->client->addResponse('GET', 'abcdef1234567890', 'accounts/empty.xml');
 
@@ -10,17 +17,26 @@ class Recurly_BaseTest extends Recurly_TestCase {
     $this->assertNull($account);
   }
 
+  public function testPassingClientToPagerItems() {
+    $this->client->addResponse('GET', 'subscriptions', 'subscriptions/index-200.xml');
+
+    $subscriptions = Recurly_Base::_get('subscriptions', $this->client);
+    $this->assertInstanceOf('Recurly_SubscriptionList', $subscriptions);
+    $this->assertEquals(getProtectedProperty($subscriptions, '_client'), $this->client);
+
+    $subscription = $subscriptions->current();
+    $this->assertInstanceOf('Recurly_Subscription', $subscription);
+    $this->assertEquals(getProtectedProperty($subscription, '_client'), $this->client);
+  }
+
   public function testPassingClientToStub() {
     $this->client->addResponse('GET', 'abcdef1234567890', 'adjustments/show-200.xml');
 
     $adjustment = Recurly_Base::_get('abcdef1234567890', $this->client);
     $this->assertInstanceOf('Recurly_Adjustment', $adjustment);
-    $this->assertInstanceOf('Recurly_Stub', $adjustment->invoice);
 
-    // The client is protected so we do a little song and dance to access it:
-    $reflection = new \ReflectionClass($adjustment->invoice);
-    $property = $reflection->getProperty('_client');
-    $property->setAccessible(true);
-    $this->assertEquals($property->getValue($adjustment->invoice), $this->client);
+    $invoice = $adjustment->invoice;
+    $this->assertInstanceOf('Recurly_Stub', $invoice);
+    $this->assertEquals(getProtectedProperty($invoice, '_client'), $this->client);
   }
 }
