7013b45d6b20ce7d0d4114b464c8fe0fabadad0a
Dania Simmons <dania02525@gmail.com>
2017-03-24 11:15:11 -0600

    add trial requires billing info field and no billing info reason field

--- Tests/Recurly/Plan_Test.php
+++ Tests/Recurly/Plan_Test.php
@@ -22,6 +22,7 @@ class Recurly_PlanTest extends Recurly_TestCase
     $this->assertEquals('days', $plan->trial_interval_unit);
     $this->assertEquals(6, $plan->total_billing_cycles);
     $this->assertEquals("Setup Fee AC", $plan->setup_fee_accounting_code);
+    $this->assertEquals(false, $plan->trial_requires_billing_info);
 
     $this->assertInstanceOf('Recurly_CurrencyList', $plan->unit_amount_in_cents);
     $this->assertEquals(1000, $plan->unit_amount_in_cents['USD']->amount_in_cents);
@@ -46,10 +47,11 @@ class Recurly_PlanTest extends Recurly_TestCase
     $plan->unit_amount_in_cents->addCurrency('USD', 1500);
     $plan->unit_amount_in_cents->addCurrency('EUR', 1200);
     $plan->setup_fee_in_cents->addCurrency('EUR', 500);
+    $plan->trial_requires_billing_info = false;
     $plan->total_billing_cycles = 6;
 
     $this->assertEquals(
-      "<?xml version=\"1.0\"?>\n<plan><plan_code>platinum</plan_code><name>Platinum &amp; Gold Plan</name><unit_amount_in_cents><USD>1500</USD><EUR>1200</EUR></unit_amount_in_cents><setup_fee_in_cents><EUR>500</EUR></setup_fee_in_cents><total_billing_cycles>6</total_billing_cycles></plan>\n",
+      "<?xml version=\"1.0\"?>\n<plan><plan_code>platinum</plan_code><name>Platinum &amp; Gold Plan</name><unit_amount_in_cents><USD>1500</USD><EUR>1200</EUR></unit_amount_in_cents><setup_fee_in_cents><EUR>500</EUR></setup_fee_in_cents><total_billing_cycles>6</total_billing_cycles><trial_requires_billing_info>false</trial_requires_billing_info></plan>\n",
       $plan->xml()
     );
   }
@@ -63,10 +65,11 @@ class Recurly_PlanTest extends Recurly_TestCase
     $plan->setup_fee_in_cents->addCurrency('EUR', 500);
     $plan->total_billing_cycles = NULL;
     $plan->tax_exempt = false;
+    $plan->trial_requires_billing_info = false;
     $plan->tax_code = 'fake-tax-code';
 
     $this->assertEquals(
-      "<?xml version=\"1.0\"?>\n<plan><plan_code>platinum</plan_code><name>Platinum Plan</name><unit_amount_in_cents><USD>1500</USD><EUR>1200</EUR></unit_amount_in_cents><setup_fee_in_cents><USD>500</USD><EUR>500</EUR></setup_fee_in_cents><total_billing_cycles nil=\"nil\"></total_billing_cycles><tax_exempt>false</tax_exempt><tax_code>fake-tax-code</tax_code></plan>\n",
+      "<?xml version=\"1.0\"?>\n<plan><plan_code>platinum</plan_code><name>Platinum Plan</name><unit_amount_in_cents><USD>1500</USD><EUR>1200</EUR></unit_amount_in_cents><setup_fee_in_cents><USD>500</USD><EUR>500</EUR></setup_fee_in_cents><total_billing_cycles nil=\"nil\"></total_billing_cycles><tax_exempt>false</tax_exempt><tax_code>fake-tax-code</tax_code><trial_requires_billing_info>false</trial_requires_billing_info></plan>\n",
       $plan->xml()
     );
   }
