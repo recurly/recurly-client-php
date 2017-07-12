9a1d3f122d27c098227b35fda88a91139dadfc67
Benjamin Eckel <bhelx@simst.im>
2017-07-12 12:54:28 -0500

    Update the version property
    Looks like we forgot to update this property on the last release. It
    should not cause a problem and I don't think we need to re-release. The
    only problem this will cause is that useragents will be mis-reporting
    this version.

--- lib/recurly/client.php
+++ lib/recurly/client.php
@@ -44,7 +44,7 @@ class Recurly_Client
    */
   private $_acceptLanguage = 'en-US';
 
-  const API_CLIENT_VERSION = '2.8.0.rc1';
+  const API_CLIENT_VERSION = '2.8.0';
   const DEFAULT_ENCODING = 'UTF-8';
 
   const GET = 'GET';
