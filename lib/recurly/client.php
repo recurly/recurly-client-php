1934d97d608f2be1d57f47dc8e46102dce0b5418
Benjamin Eckel <bhelx@simst.im>
2017-10-06 11:51:05 -0500

    Bump 2.9.0

--- lib/recurly/client.php
+++ lib/recurly/client.php
@@ -44,7 +44,7 @@ class Recurly_Client
    */
   private $_acceptLanguage = 'en-US';
 
-  const API_CLIENT_VERSION = '2.8.2';
+  const API_CLIENT_VERSION = '2.9.0';
   const DEFAULT_ENCODING = 'UTF-8';
 
   const GET = 'GET';
