d1375560ef2b49d18ca1e771a2d321c8e76100b2
Benjamin Eckel <bhelx@simst.im>
2018-06-01 16:40:30 -0500

    Allow programmer to set gateway code in purchase

--- lib/recurly/purchase.php
+++ lib/recurly/purchase.php
@@ -14,6 +14,7 @@
  * @property string $terms_and_conditions Optional Terms and Conditions field. This will default to the Terms and Conditions text specified on the Invoice Settings page in your Recurly admin. Custom notes will stay with a subscription on all renewals.
  * @property string $vat_reverse_charge_notes Optional VAT Reverse Charge Notes only appear if you have EU VAT enabled or are using your own Avalara AvaTax account and the customer is in the EU, has a VAT number, and is in a different country than your own. This will default to the VAT Reverse Charge Notes text specified on the Tax Settings page in your Recurly admin, unless custom notes were created with the original subscription. Custom notes will stay with a subscription on all renewals.
  * @property integer $shipping_address_id Optional id of an existing ShippingAddress to be applied to all subscriptions and adjustments in purchase.
+ * @property string $gateway_code Optional base36 encoded id for the gateway you wish to use for this transaction.
  */
 class Recurly_Purchase extends Recurly_Resource
 {
@@ -79,7 +80,8 @@ class Recurly_Purchase extends Recurly_Resource
     return array(
       'account', 'adjustments', 'collection_method', 'currency', 'po_number',
       'net_terms', 'subscriptions', 'gift_card', 'coupon_codes', 'customer_notes',
-      'terms_and_conditions', 'vat_reverse_charge_notes', 'shipping_address_id'
+      'terms_and_conditions', 'vat_reverse_charge_notes', 'shipping_address_id',
+      'gateway_code'
     );
   }
 }
