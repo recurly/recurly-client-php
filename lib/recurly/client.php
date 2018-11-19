e472e41b75b3179e8ac07ff48ea874a6f875a326
Aaron Suarez <asuarez@recurly.com>
2018-11-19 13:40:03 -0600

    Bump to API version 2.17

--- lib/recurly/client.php
+++ lib/recurly/client.php
@@ -27,7 +27,7 @@ class Recurly_Client
   /**
    * API Version
    */
-  public static $apiVersion = '2.16';
+  public static $apiVersion = '2.17';
 
   /**
    * The path to your CA certs. Use only if needed (if you can't fix libcurl/php).
