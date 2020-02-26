8742ffdca4b87aebba6638df91563843baa847fe
Doug Miller <doug@douglas-miller.com>
2020-02-26 12:16:19 -0600

    Adding PHP_EOL to strict mode messaging

--- lib/recurly.php
+++ lib/recurly.php
@@ -5,5 +5,5 @@ namespace Recurly;
 define('Recurly\STRICT_MODE', getenv("RECURLY_STRICT_MODE") == 'true');
 
 if (STRICT_MODE) {
-    echo "[Recurly] [WARNING] STRICT_MODE enabled. This should only be used for testing purposes.";
+    echo "[Recurly] [WARNING] STRICT_MODE enabled. This should only be used for testing purposes." . PHP_EOL;
 }
