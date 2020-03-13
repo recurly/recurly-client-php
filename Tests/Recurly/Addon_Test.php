5048861a7966dc7101d1ea92863c296af5e33e3f
Joanna Sese <jmgsese@gmail.com>
2020-03-13 14:30:43 -0500

    Add test

--- Tests/Recurly/Addon_Test.php
+++ Tests/Recurly/Addon_Test.php
@@ -30,4 +30,15 @@ class Recurly_AddonTest extends Recurly_TestCase
     $this->assertInstanceOf('Recurly_Addon', $addon);
     $addon->delete();
   }
+
+  public function testCreateXmlItemBackedAddOn() {
+    $item = new Recurly_Item();
+    $item->item_code = 'little_llama';
+    $addon = new Recurly_Addon();
+    $addon->plan_code = 'gold';
+    $addon->item_code = $item->item_code;
+    $addon->unit_amount_in_cents->addCurrency('USD', 400);
+
+    $this->assertEquals("<?xml version=\"1.0\"?>\n<add_on><item_code>little_llama</item_code><unit_amount_in_cents><USD>400</USD></unit_amount_in_cents></add_on>\n", $addon->xml());
+  }
 }
