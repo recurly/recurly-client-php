0e69a9f766ed554e8459c037e7e9c9929294b6b3
Doug Miller <doug@douglas-miller.com>
2020-10-01 14:16:16 -0500

    Updating client to be compliant with RFC 2616: case-insensitive headers

--- lib/recurly/client.php
+++ lib/recurly/client.php
@@ -44,7 +44,7 @@ class Recurly_Client
    */
   private $_acceptLanguage = 'en-US';
 
-  const API_CLIENT_VERSION = '2.6.0';
+  const API_CLIENT_VERSION = '2.6.1';
   const DEFAULT_ENCODING = 'UTF-8';
 
   const GET = 'GET';
@@ -188,8 +188,10 @@ class Recurly_Client
     $returnHeaders = array();
     foreach ($headers as &$header) {
       preg_match('/([^:]+): (.*)/', $header, $matches);
-      if (sizeof($matches) > 2)
-        $returnHeaders[$matches[1]] = $matches[2];
+      if (sizeof($matches) > 2) {
+        $headerKey = strtolower($matches[1]);
+        $returnHeaders[$headerKey] = $matches[2];
+      }
     }
     return $returnHeaders;
   }
