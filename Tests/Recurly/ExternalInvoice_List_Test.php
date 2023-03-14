df7d5b4ff7bb29c1b62ae6808055acecd4bde2fd
Amanda Fielding <amandamfielding@gmail.com>
2023-03-14 11:52:43 -0700

    v2-external-invoices

---
+++ Tests/Recurly/ExternalInvoice_List_Test.php
@@ -0,0 +1,29 @@
+<?php
+
+
+class Recurly_ExternalInvoiceListTest extends Recurly_TestCase
+{
+
+  public function testGetAll() {
+    $url = '/external_invoices';
+    $this->client->addResponse('GET', $url, 'external_invoices/index-200.xml');
+
+    $external_invoices = Recurly_ExternalInvoiceList::get(null, $this->client);
+    $this->assertInstanceOf('Recurly_ExternalInvoiceList', $external_invoices);
+    $this->assertEquals($url, $external_invoices->getHref());
+
+    $external_invoice = $external_invoices->current();
+    $this->assertInstanceOf('Recurly_ExternalInvoice', $external_invoice);
+    $this->assertInstanceOf('Recurly_Stub', $external_invoice->account);
+    $this->assertInstanceOf('Recurly_Stub', $external_invoice->external_subscription);
+    $this->assertEquals($external_invoice->external_id, 'external-id');
+    $this->assertEquals($external_invoice->state, 'paid');
+    $this->assertEquals($external_invoice->currency, 'USD');
+    $this->assertEquals($external_invoice->total, 100);
+    $external_charge = $external_invoice->line_items[0];
+    $this->assertEquals($external_charge->unit_amount, 50);
+    $this->assertInstanceOf('DateTime', $external_invoice->purchased_at);
+    $this->assertInstanceOf('DateTime', $external_invoice->created_at);
+    $this->assertInstanceOf('DateTime', $external_invoice->updated_at);
+  }
+}
---
+++ lib/recurly/external_invoice.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * class Recurly_ExternalInvoice
+ * @property Recurly_Stub $account
+ * @property Recurly_Stub $external_subscription
+ * @property Recurly_ExternalCharge[] $line_items
+ * @property string $external_id
+ * @property boolean $state
+ * @property integer $total_in_cents
+ * @property string $currency
+ * @property DateTime $purchased_at
+ * @property DateTime $created_at
+ * @property DateTime $updated_at
+ */
+class Recurly_ExternalInvoice extends Recurly_Resource
+{
+/**
+ * @param $uuid
+ * @param Recurly_Client $client Optional client for the request, useful for mocking the client
+ * @return Recurly_ExternalInvoice|null
+ * @throws Recurly_Error
+ */
+  public static function get($uuid, $client = null) {
+    return Recurly_Base::_get(Recurly_ExternalInvoice::uriForExternalInvoice($uuid), $client);
+  }
+
+  protected static function uriForExternalInvoice($uuid) {
+    return self::_safeUri(Recurly_Client::PATH_EXTERNAL_INVOICES, $uuid);
+  }
+
+  protected function getNodeName() {
+    return 'external_invoice';
+  }
+
+  protected function getWriteableAttributes() {
+   return array();
+  }
+}
