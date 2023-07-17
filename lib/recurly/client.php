3467ea1dd63d79c00f146361b7c37278f5c77040
Lib Cat <noreply@recurly.com>
2023-07-17 16:51:16 +0000

    2.14.5
    [Full Changelog](https://github.com/recurly/recurly-client-php/compare/2.14.4...2.14.5)
    
    **Merged Pull Requests**
    
    - V2 Client Library External Subscription changes [#774](https://github.com/recurly/recurly-client-php/pull/774) ([paulorbpinho-fullstacklabs](https://github.com/paulorbpinho-fullstacklabs))

--- lib/recurly/client.php
+++ lib/recurly/client.php
@@ -51,7 +51,7 @@ class Recurly_Client
    */
   private static $apiUrl = 'https://%s.recurly.com/v2';
 
-  const API_CLIENT_VERSION = '2.14.4';
+  const API_CLIENT_VERSION = '2.14.5';
   const DEFAULT_ENCODING = 'UTF-8';
 
   const GET = 'GET';
